d1f007d0c73144142a9c9f18e7ce36b3c299f985
Yuri/Sanits <stmeoff@gmail.com>
2024-06-07 16:37:24 -0300

    codigo colocado

--- dedo.js
+++ dedo.js
@@ -1,1 +1,9 @@
 console.log("aula de github");
+class carro{
+        constructor(tipo,capacidade){
+            this.tipo = tipo;
+            this.capacidade = capacidade
+        }
+}
+
+const gol = new carro("carro",5);
